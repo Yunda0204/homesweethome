ae19b8526982e5a4312d3ce710db469e231433ff
Yunda0204 <a01034353045@gmail.com>
2024-06-12 15:18:36 +0900

    필터와 래퍼

--- 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/CommonFilter.java
+++ 9. 웹 서버 프로그램 구현/day04/src/main/java/filters/CommonFilter.java
@@ -5,13 +5,27 @@ import jakarta.servlet.*;
 import java.io.IOException;
 
 public class CommonFilter implements Filter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init()");
+        String key1 = filterConfig.getInitParameter("key1");
+        String key2 = filterConfig.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+    }
 
-        System.out.println("CommonFilter - 요청 처리 전");
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
+    }
 
-        filterChain.doFilter(servletRequest, servletResponse);
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        System.out.println("CommonFilter - 응답 후");
+        //System.out.println("CommonFilter - 요청 전");
+
+        chain.doFilter(new CommonRequestWrapper(request), response);
+
+        //System.out.println("CommonFilter - 응답 후");
     }
 }
