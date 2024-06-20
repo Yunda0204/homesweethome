3860d6823883cdba9fc09bdf9250f6c7e97efa03
Yunda0204 <a01034353045@gmail.com>
2024-06-20 21:02:36 +0900

    프로젝트3 연습
    회원가입, 로그인

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
@@ -10,9 +10,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.choongang.global.exceptions.CommonException;
 import org.choongang.member.services.JoinService;
 import org.choongang.member.services.MemberServiceProvider;
-
 import java.io.IOException;
-import java.io.PrintWriter;
+
+import static org.choongang.global.MessageUtil.alertError;
+import static org.choongang.global.MessageUtil.go;
 
 @WebServlet("/member/join")
 public class JoinController extends HttpServlet {
@@ -28,7 +29,10 @@ public class JoinController extends HttpServlet {
         try {
             JoinService service = MemberServiceProvider.getInstance().joinService();
             service.process(req);
+            go(req.getContextPath() + "/member/login", "parent", resp);
+//            resp.sendRedirect(req.getContextPath() + "/member/login");
         } catch (CommonException e) {
+            alertError(e, resp);
         }
     }
 }
--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/mapper/MemberMapper.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/mapper/MemberMapper.java
@@ -1,10 +1,15 @@
 package org.choongang.member.mapper;
 
+import org.apache.ibatis.annotations.Select;
 import org.choongang.member.entities.Member;
+
+import java.util.List;
 
 public interface MemberMapper {
     long exist(String email);
     Member get(String email);
     int register(Member member);
 
+//    @Select("SELECT * FROM MEMBER")
+//    List<Member> getListr();
 }
