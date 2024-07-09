ea20953c38bfd8977a7d1559309d9bd484f752a3
Yunda0204 <a01034353045@gmail.com>
2024-07-09 16:12:18 +0900

    spring SLF4J API Module 추가

--- 11. 프레임워크 프로그래밍/day03_02/src/test/java/exam01/Ex01.java
+++ 11. 프레임워크 프로그래밍/day03_02/src/test/java/exam01/Ex01.java
@@ -1,12 +1,18 @@
 package exam01;
 
 import config.AppCtx;
+import member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = AppCtx.class)
@@ -19,8 +25,42 @@ public class Ex01 {
     void test1() {
         String sql = "INSERT INTO MEMBER (SEQ, EMAIL, PASSWORD, USER_NAME) " +
                 " VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?)";
-        int result = jdbcTemplate.update(sql, "qwer@naver.com", "qwer1234", "도선화");
+        int result = jdbcTemplate.update(sql, "qwer12@test.org", "123456", "사용자03");
 
         System.out.println(result);
+    }
+
+    @Test
+    void test2() {
+        List<Member> members = jdbcTemplate.query("SELECT * FROM MEMBER", this::mapper);
+
+        members.forEach(System.out::println);
+    }
+
+    @Test
+    void test3() {
+        String email = "qwer12@test.org";
+        try {
+            Member member = jdbcTemplate.queryForObject("SELECT * FROM MEMBER WHERE EMAIL = ?", this::mapper, email);
+        } catch (Exception e) {
+            System.out.println("없음");
+        }
+
+    }
+
+    @Test
+    void test4() {
+        int total = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM MEMBER", int.class);
+        System.out.println(total);
+    }
+
+    private Member mapper(ResultSet rs, int num) throws SQLException {
+        return Member.builder()
+                .seq(rs.getLong("SEQ"))
+                .email(rs.getString("EMAIL"))
+                .password(rs.getString("PASSWORD"))
+                .userName(rs.getString("USER_NAME"))
+                .regDt(rs.getTimestamp("REG_DT").toLocalDateTime())
+                .build();
     }
 }
