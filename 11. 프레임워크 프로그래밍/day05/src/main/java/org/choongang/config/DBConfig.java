4c1901732b34ab53a93d2a0dd2571e183a68b205
Yunda0204 <a01034353045@gmail.com>
2024-07-11 10:58:17 +0900

    day05

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -1,10 +1,20 @@
 package org.choongang.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
+@MapperScan("org.choongang")
 public class DBConfig {
 
     @Bean(destroyMethod = "close")
@@ -28,6 +38,25 @@ public class DBConfig {
         /* 커넥션 풀 설정 E */
 
         return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() { // mybatis 쓰면 필요없음
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+
+        return factoryBean.getObject();
     }
 
 }
